c01b862630940bdc2442fbc888c99b9dec4ebfa4
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2024-01-17 17:40:53 +0900

    feat: Implement Component Common(Header, Footer)

---
+++ client/src/components/common/styles.tsx
@@ -0,0 +1,26 @@
+import styled from "tailwind-styled-components";
+
+const Main = styled.div`
+    flex
+    flex-col
+    w-[850px]
+    h-screen
+    overflow-hidden
+    rounded-sm
+    bg-white
+    border
+    border-white/10
+`
+const Header=styled.div`
+    flex
+    justify-between
+    items-center
+    h-10
+    pl-4
+`
+const Footer = styled.div`
+    flex
+    h-[5%]
+    bg-[#d9d9d9]
+    mt-auto
+`
