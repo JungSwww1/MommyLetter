741db2ecb199c434f31d5c253712227e91193616
mdhycho <stevenjung1213@gmail.com>
2024-01-18 16:32:57 +0900

    Update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {AppMain} from'./styles';
-import {Routes, Route} from 'react-router-dom';
+import React from "react";
+import { AppMain } from "./styles";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "@/pages/LoginPage";
 import UserRegist from "@/pages/UserRegist";
 import UserProfile from "@/pages/UserProfile";
@@ -8,10 +8,10 @@ function App() {
   return (
     <AppMain>
       <Routes>
-        <Route path="/" element={<LoginPage/>}/>
-        <Route path="/signup" element={<UserRegist/>}/>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<UserRegist />} />
         {/* 차후에 userId에 관한 것 추가*/}
-        <Route path="/profiles/{userId}" element={<UserProfile/>}/>
+        <Route path="/profiles/{userId}" element={<UserProfile />} />
       </Routes>
     </AppMain>
   );
