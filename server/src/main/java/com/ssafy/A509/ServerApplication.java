28103b915039018f0c3bc776d6d84133f6738252
KimDohaAcc <8772158@naver.com>
2024-01-18 14:34:50 +0900

    :arrow_up: feat: set modelMapper and add dependency

--- server/src/main/java/com/ssafy/A509/ServerApplication.java
+++ server/src/main/java/com/ssafy/A509/ServerApplication.java
@@ -2,7 +2,9 @@ package com.ssafy.A509;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ServerApplication {
 
--- server/src/main/java/com/ssafy/A509/config/AppConfig.java
+++ server/src/main/java/com/ssafy/A509/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.A509.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration.AccessLevel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,13 @@ public class AppConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration()
+			// private 이어도 접근할 수 있음
+			.setFieldAccessLevel(AccessLevel.PRIVATE)
+			// 필드명 같으면 자동 매핑 처리
+			.setFieldMatchingEnabled(true);
+
+		return modelMapper;
 	}
 }
