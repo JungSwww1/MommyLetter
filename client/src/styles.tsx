4a25979387810c97f9ce8a2730268e550e5eff6f
mdhycho <stevenjung1213@gmail.com>
2024-01-19 10:49:18 +0900

    Feat(UserProfile.tsx): Implement UserProfile
    사용자 프로필 화면

--- client/src/styles.tsx
+++ client/src/styles.tsx
@@ -2,10 +2,11 @@ import styled from "tailwind-styled-components";
 
 export const AppMain = styled.div`
   w-[850px]
-  h-screen 
-  relative 
+  h-screen
+  relative
   rounded-sm 
-  bg-white border border-white/10 
-  min-h-[320px]
+  bg-white border border-white/10
+  max-h-[739px] overflow-y-scroll
+  max-w-[850px] overflow-x-hidden
 `
 
