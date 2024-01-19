223b0fdd3efd87867331a3b2d28ab1865d082813
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2024-01-19 13:24:21 +0900

    💄Style(App.tsx): 디스플레이 Flex로 변경

--- client/src/styles.tsx
+++ client/src/styles.tsx
@@ -1,7 +1,11 @@
 import styled from "tailwind-styled-components";
 
 export const AppMain = styled.div`
-  w-[850px]
-    h-screen relative rounded-sm bg-white border border-white/10 min-h-[320px]
+  flex-col
+  w-[50%]
+  h-screen
+  bg-white 
+  max-h-[739px] overflow-y-scroll
+  max-w-[850px] overflow-x-hidden
 `
 
