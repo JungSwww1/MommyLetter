0e65787e596d24b47b988161df1ffe1e0a3a663a
김민석 <tntjd9514@gmail.com>
2024-01-22 13:03:50 +0900

    :art: Fix: Correct user_info MVC 코드 수정

--- server/src/main/java/com/ssafy/A509/account/dto/CreateAccountRequest.java
+++ server/src/main/java/com/ssafy/A509/account/dto/CreateAccountRequest.java
@@ -11,7 +11,12 @@ import lombok.*;
 @Builder
 public class CreateAccountRequest {
 
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
+
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
     private String nickname;
+
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
 }
--- server/src/main/java/com/ssafy/A509/account/model/User.java
+++ server/src/main/java/com/ssafy/A509/account/model/User.java
@@ -1,7 +1,5 @@
 package com.ssafy.A509.account.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.ssafy.A509.board.model.Board;
 import jakarta.persistence.*;
 
@@ -9,15 +7,13 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @EntityListeners(AuditingEntityListener.class)
@@ -70,15 +66,4 @@ public class User {
     this.role = role;
     }
 
-    public void setNickname(String nickname) {
-      this.nickname = nickname;
-    }
-
-    public void setIntro(String intro) {
-      this.intro = intro;
-    }
-
-    public void setPassword(String password) {
-      this.password = password;
-    }
 }
