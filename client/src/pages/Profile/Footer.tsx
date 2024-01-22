9a234cd10bd40a4375b37021d096bc78950c7994
mdhycho <stevenjung1213@gmail.com>
2024-01-22 15:52:32 +0900

    ✨Feat: Implement UserEdit.tsx
    회원 정보 수정페이지 구현 및 토글 스위치 디자인 수정

---
+++ client/src/pages/Profile/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return(
+        <div className={"flex h-[100px] bottom-0 sticky bg-gray-300"}>
+            This is Footer
+        </div>
+    )
+}
+export default Footer;
--- client/src/pages/UserEdit/UserEdit.tsx
+++ client/src/pages/UserEdit/UserEdit.tsx
@@ -1,23 +1,33 @@
 import logo from '@/assets/sample1.jpg'
-import {ProfileImg} from "@/pages/UserEdit/styles";
+import {
+    Container,
+    Input,
+    ProfileImg,
+    ProfileWrapper,
+    StyleLink, SubContainer, Submit,
+    Title,
+    Wrapper,
+    Wrapper2, Wrapper3
+} from "@/pages/UserEdit/styles";
 import {Link} from "react-router-dom"
+import Header from "@/pages/Profile/Header";
+import Footer from "@/pages/Profile/Footer";
 
 const UserEdit = () => {
     return (
-        <div className="">
-            <nav className="bg-gray-300 mt-[5%] mb-[5%]">This is header</nav>
-            <div className="bg-amber-200 mt-[10%]">
+        <div>
+            <Header/>
+            <Container>
                 {/* 하단의 헤더 마진값은 추후에 조정해야 한다. */}
-                
 
                 {/* Profile IMG */}
-                <div className="flex justify-center">
+                <ProfileWrapper>
                     <ProfileImg
                         src={logo}
                         alt="Logo"
                     />
-                </div>
-                
+                </ProfileWrapper>
+
                 {/* Line under profile img */}
                 <svg
                     viewBox="0 0 385 1"
@@ -25,31 +35,34 @@ const UserEdit = () => {
                     xmlns="http://www.w3.org/2000/svg"
                     preserveAspectRatio="xMidYMid meet"
                     className='mb-[5%]'
-                    >
-                    <line x1="-4.37114e-8" y1="0.500031" x2={385} y2="0.499997" stroke="black" />
+                >
+                    <line x1="-4.37114e-8" y1="0.500031" x2={385} y2="0.499997" stroke="black"/>
                 </svg>
-                
+
                 {/* nickname and intro */}
-                <div className="flex mb-[2%] justify-around">
-                    <div>닉네임</div>
-                    <input type="text" className="w-[59%] ml-[35px] border-b-2 border-black"/>
-                </div>
-                <div className="flex mb-[2%] justify-around">
-                    <div>소개</div>
-                    <input type="text" className="w-[59%] ml-[50px]"/>
-                </div>
+                <Wrapper2>
+                    <Title>닉네임</Title>
+                    <Input type="text"/>
+                </Wrapper2>
+                <Wrapper2>
+                    <Title>소개</Title>
+                    <Input type="text"/>
+                </Wrapper2>
+                <Wrapper3>
+                    <Submit>수정</Submit>
+                </Wrapper3>
 
                 {/* password change and user withdrawal */}
-                <div className="mt-[10%]">
-                    <div className="ml-[7%] mb-[2%]">
-                        <Link to = "/passwordChange">비밀번호 변경</Link>
-                    </div>
-                    <div className="ml-[7%] mb-[2%]">
-                        <Link to ="/withdrawal">회원탈퇴</Link>
-                    </div>
-                </div>
-            </div> 
-
+                <SubContainer>
+                    <Wrapper>
+                        <StyleLink to="/passwordChange">비밀번호 변경</StyleLink>
+                    </Wrapper>
+                    <Wrapper>
+                        <StyleLink to="/withdrawal">회원탈퇴</StyleLink>
+                    </Wrapper>
+                </SubContainer>
+            </Container>
+            <Footer/>
         </div>
     )
 }
