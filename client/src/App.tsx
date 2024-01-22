5c25fd200a51c35d4e7667df6fc7b0a676a53ddd
mdhycho <stevenjung1213@gmail.com>
2024-01-22 15:50:26 +0900

    💄Design: index.html
    index.html 탭에서 보이는 제목 밑 사진 디자인 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {AppMain} from'./styles';
 import {Routes, Route} from 'react-router-dom';
-import LoginPage from "@/pages/LoginPage";
-import UserRegist from "@/pages/UserRegist";
-import {DiaryPage,DiaryWrite} from "@/components/Diary"
-import {Diary} from "@/components/Diary/styles";
+import LoginPage from "@/pages/LoginPage/LoginPage";
+
 function App() {
   return (
     <AppMain>
-      <DiaryWrite/>
+      <Routes>
+          <Route path={"/"} element={<LoginPage/>}/>
+      </Routes>
         
     </AppMain>
   );
