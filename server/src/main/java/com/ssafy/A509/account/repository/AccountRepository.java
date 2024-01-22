08d5b877d17cef705132d654d3b9353761961a84
김민석 <tntjd9514@gmail.com>
2024-01-22 10:40:25 +0900

    :ambulance: Fix: Account MVC REST API 수정

--- server/src/main/java/com/ssafy/A509/account/repository/AccountRepository.java
+++ server/src/main/java/com/ssafy/A509/account/repository/AccountRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AccountRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
-
     boolean existsByNickname(String nickname);
 }
