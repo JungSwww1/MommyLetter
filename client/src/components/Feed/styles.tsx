4421e6cc6044cbc0124d12af84be3d1ea8c9cd36
mdhycho <stevenjung1213@gmail.com>
2024-01-23 13:28:17 +0900

    🔨Refactor: Feed.tsx
    피드 메인 페이지 화면 코드 수정 및 추가

--- client/src/components/Feed/styles.tsx
+++ client/src/components/Feed/styles.tsx
@@ -6,3 +6,38 @@ export const Layout = styled.div`
  ml-[3%]
  mr-[3%]   
 `
+
+export const TitleContainer = styled.div`
+    flex 
+    mb-[3%] 
+    justify-between
+`
+export const ContextContainer = styled.div`
+    mb-[3%] 
+    bg-gray-300 
+    p-4 
+    rounded-md 
+    overflow-hidden
+`
+export const PhotoContainer = styled.div`
+    flex
+`
+
+export const HashtagContainer = styled.div`
+    mb-[2%]
+`
+export const LikeIconContainer = styled.div`
+    flex 
+    mb-[3%] 
+    justify-between
+`
+export const CommentContainer = styled.div`
+    flex 
+    space-x-4
+`
+
+export const TitleWrapper = styled.div`
+    flex 
+    items-center 
+    space-x-4
+`
