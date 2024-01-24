48e954d337a52d9f02d5f1904fd0f85f8bc07f97
mdhycho <stevenjung1213@gmail.com>
2024-01-24 17:06:12 +0900

    ✨Feat: Implement persisted
    1. 여러 reducer들에 대해서도 적용 가능하도록 구현 완료
    2. 해당되는 내용들 다시 수정

--- client/src/stores/store.tsx
+++ client/src/stores/store.tsx
@@ -1,15 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import authReducer from './User/authSlice';
 import storage from 'redux-persist/lib/storage';
 import { persistStore, persistReducer } from "redux-persist";
 
+// 아래는 localStorage에 적용될 내용 : "root"라는 이름으로 storage, 즉 localStorage에 저장한다.
+// 최종적으로 저장될 때는 persist(라이브러리의 네임스페이스):root(본인이 설정한 것)로 표시된다.
 const persistConfig = {
     key: "root",
     storage,
 };
 
+const rootReducer = combineReducers({
+    //authSlice에서 정의된 name을 authReducer로 임의로 reducer에 이름을 붙인 것이다.
+    signup:authReducer
+})
+
 // rootReducer에 대한 persist 설정을 적용
-const persistedReducer = persistReducer(persistConfig, authReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
     reducer: persistedReducer,
