47f7699f3f266ac631e45e46a529d2a2ff4f0857
mdhycho <stevenjung1213@gmail.com>
2024-01-26 11:09:15 +0900

    💄Design: UserRegist.tsx
    회원 가입 페이지 디자인 수정

--- client/src/pages/UserRegist/UserRegist.tsx
+++ client/src/pages/UserRegist/UserRegist.tsx
@@ -1,4 +1,12 @@
-import {CheckButton, DefaultInput, PasswordInput, RegisterButton, Title} from "@/pages/UserRegist/styles";
+import {
+    CheckButton,
+    DefaultInput,
+    Layout,
+    EmailInput,
+    RegisterButton,
+    Title,
+    Container, EmailWrapper, RegistContainer, MainTitle
+} from "@/pages/UserRegist/styles";
 import {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "@/stores/store";
@@ -39,78 +47,68 @@ const UserRegist =()=> {
     }
 
     return(
-        <div>
-            <div>
-                {Storedpassword}
-            </div>
-                <div>
-                {Storedemail}
-                </div>
-                {Storednickname}
-            <div className="relative left-[25%] top-[-50px]">
-                <Title className="left-[23%] top-[131px]">
-                    회원가입
-                </Title>
+        <Layout>
+            {/*<div>*/}
+            {/*    {Storedpassword}*/}
+            {/*</div>*/}
+            {/*<div>*/}
+            {/*    {Storedemail}*/}
+            {/*</div>*/}
+            {/*{Storednickname}*/}
+            <MainTitle>회원가입</MainTitle>
 
-                {/*이메일*/}
-                <Title className="left-[3%] top-48">이메일</Title>
+            <Container>
+                <Title>이메일</Title>
+                <EmailWrapper>
+                    <EmailInput type="text"
+                           placeholder="이메일"
+                           onChange={(e) => setLocalEmail1(e.target.value)}
+                    />
+                    <p className={"font-bold"}>@</p>
+                    <EmailInput type="text"
+                           placeholder="example.com"
+                           onChange={(e) => setLocalEmail2(e.target.value)}
+                    />
+                </EmailWrapper>
+                <CheckButton>중복확인</CheckButton>
+            </Container>
+
+            <Container>
+                <Title>닉네임</Title>
                 <DefaultInput type="text"
-                       placeholder="이메일"
-                       className="w-[22%] left-[3%] top-[236px] mr-[0.1%]"
-                       onChange={(e)=>setLocalEmail1(e.target.value)}
+                              placeholder="닉네임을 입력해주세요"
+                              onChange={(e)=>setLocalNickname(e.target.value)}
                 />
-                <DefaultInput type="text"
-                       placeholder="example.com"
-                       className="w-[24%] left-[30%] top-[236px] ml-[0.1%]"
-                       onChange={(e)=>setLocalEmail2(e.target.value)}
+                <CheckButton>중복확인</CheckButton>
+            </Container>
 
-                />
-                <Title className="w-[20%] left-[26.0%] top-[240px] text-[20px] text-base ml-[0.1%] mr-[0.1%]"
-                >@</Title>
-                <CheckButton onClick={checkEmail}
-                        className="left-[3%] top-[281px]">중복확인
-                </CheckButton>
-
-                {/*닉네임*/}
-                <Title className="left-[3%] top-[336px]">닉네임</Title>
-                <DefaultInput type="text"
-                       placeholder="닉네임을 입력해주세요"
-                       className="w-[35%] left-[3%] top-[381px]"
-                       onChange={(e)=>setLocalNickname(e.target.value)}
-
-                />
-                <CheckButton onClick={checkID}
-                        className="w-[13%] left-[42%] top-[380px]">중복확인
-                </CheckButton>
-
-                {/*비밀번호*/}
-                <Title className="left-[3%] top-[482px]">
-                    비밀번호
-                </Title>
-                <PasswordInput type="password"
+            <Container>
+                <Title>비밀번호</Title>
+                <DefaultInput type="password"
                        placeholder="비밀번호를 입력해주세요"
-                       className="top-[535px]"
                        onChange={(e)=>setLocalPassword(e.target.value)}
-
                 />
-                <PasswordInput type="password"
+                <Title>비밀번호 확인</Title>
+                <DefaultInput type="password"
                        placeholder="비밀번호를 다시 입력해주세요"
-                       className="top-[583px]"
                        onChange={(e) => setConfirmPassword(e.target.value)}
                 />
-
-                <div className={"top-[610px]"}>
+                <div>
                 {!passwordMatch && (
                     <div className={"text-red-500"}>
                         비밀번호가 일치하지 않습니다.
                     </div>
                 )}
                 </div>
+            </Container>
+
+            <RegistContainer>
                 <RegisterButton onClick={register}>
                     회원가입
                 </RegisterButton>
-            </div>
-        </div>
+            </RegistContainer>
+        </Layout>
+
     )
 }
 
