309d10c7dc8ca7d5e19b25593814510fcd04d421
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2024-01-30 12:37:19 +0900

    ✨Feat: 상담 Link추가
    햄버거바에 상담 Link추가

--- client/src/components/HamburgerButton/index.tsx
+++ client/src/components/HamburgerButton/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {HamburgerLayout,List,Item,Button} from "./styles"
 import {useNavigate} from "react-router-dom";
 import Hamburger from "@/assets/icons/Hamburger";
-
+import {Link} from 'react-router-dom'
 
 const HamburgerButton = () => {
     const navigate = useNavigate();
@@ -17,9 +17,11 @@ const HamburgerButton = () => {
             <List tabIndex={0}>
                 {/*Consult에 있는 Card리팩토링 후 재사용하여 넣을 예정*/}
                 <h1>프로필 카드가 들어갈 곳</h1>
+                <Link to="/search"><Item><a>검색</a></Item></Link>
+                <Link to="/consult"><Item><a>상담하기</a></Item></Link>
+                <Link to="/history"><Item><a>상담기록</a></Item></Link>
                 <Item><a>회원정보수정</a></Item>
                 <Item><a>상담정보 등록/수정</a></Item>
-                <Item><a>상담 기록</a></Item>
                 <Button onClick={logout}>로그아웃 들어가자</Button>
             </List>
         </HamburgerLayout>
