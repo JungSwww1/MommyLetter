5f288d56567249459834ce41b44913664b5e6bcb
SeungminKo <smink95@naver.com>
2024-02-06 10:44:08 +0900

    :bug: Modify : CrossOrigin duplicate settings

--- server/src/main/java/com/ssafy/A509/diary/controller/DiaryController.java
+++ server/src/main/java/com/ssafy/A509/diary/controller/DiaryController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin("*")
 @RequiredArgsConstructor
 @RequestMapping("/diary")
 @Tag(name = "Diary", description = "Diary API")
