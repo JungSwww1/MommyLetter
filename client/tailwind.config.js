7ddadc890ca996fa97505d9f212307c3926b1d4e
정승원 <129469541+JungSwww1@users.noreply.github.com>
2024-02-07 00:02:04 +0900

    💄Design: LoginPage design changed, added style codes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
         'doctor': '#3854E9',
         'MenuColor':"#533C00",
       },
+      boxShadow: {
+        'custom-inner': 'inset 0 5px 5px 2px rgba(0, 0, 0, 0.2)',
+        'custom-outer': '3px 3px 10px 4px rgba(0, 0, 0, 0.25)',
+      },
     },
   },
   plugins: [require("daisyui")],
