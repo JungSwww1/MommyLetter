5d17185d36980319e0af161484a98f22b3f241c6
SeungminKo <smink95@naver.com>
2024-02-08 13:33:53 +0900

    :bug: import class

--- server/src/main/java/com/ssafy/A509/dm/repository/DMRepository.java
+++ server/src/main/java/com/ssafy/A509/dm/repository/DMRepository.java
@@ -1,6 +1,8 @@
 package com.ssafy.A509.dm.repository;
 
 import com.ssafy.A509.dm.model.DirectMessage;
+
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Sort;
