210e43f70e227ee74cef097410cf844d755d7904
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2024-02-11 19:33:37 +0900

    🔨Refactor: 사용하지 않는 코드 제거, 코드 간소화

--- client/src/components/DiaryList/index.tsx
+++ client/src/components/DiaryList/index.tsx
@@ -1,41 +1,48 @@
-import React from "react";
-import {DiaryResponseProps,PhotoListProps} from "@/components/type/types";
+import React, {useState} from "react";
+import {DiaryResponseProps} from "@/components/type/types";
 
-
-export const DiaryListComponent = ({content,emoji,photoList,createdDate }:DiaryResponseProps) => {
+export const DiaryListComponent = ({content, emoji, photoList, createdDate}: DiaryResponseProps) => {
+    const [modal, setModal] = useState(false);
     const date = new Date(createdDate);
-    var emojiArr=[];
-    emojiArr[0] = "/assets/images/sample_angry.png";
-    emojiArr[1] = "/assets/images/sample_bad.png";
-    emojiArr[2] = "/assets/images/sample_good.png";
-    emojiArr[3] = "/assets/images/sample_soso.png";
-    emojiArr[4] = "/assets/images/sample_tired.png";
-
-    const letterCheck = (num:number) =>{
+    var emojiArr: string[] = [];
+    const path = "/assets/images/"
+    const emotionImg = ["1sad.png", "2lonely.png", "3irritated.png", "4tired.png", "5angry.png", "6soso.png", "7delight.png", "8calm.png", "9delight.png", "10excited.png"]
+    for (let i = 0; i < 10; i++) {
+        emojiArr[i] = path + emotionImg[i];
+    }
+    const letterCheck = (num: number) => {
         const parseNum = num.toString();
 
         return parseNum.length < 2 ? "0" + parseNum : parseNum;
     }
+
     return (
-        <div className="p-5">
-            <div>
-                <div className="flex justify-between">
-                    <span className="font-bold">{date.getFullYear()}. {letterCheck(date.getMonth()+1)}.{letterCheck(date.getDate())}</span>
-                    <img className="w-[10%]" src={emojiArr[emoji]}/>
+
+        <div className="w-[100%] p-5 relative bottom-10 z-0">
+
+
+            <div className="flex w-[100%]">
+                <div className="flex flex-col w-[25%]">
+                    <span
+                        className="font-bold text-pointColor">{letterCheck(date.getMonth() + 1)}월 {letterCheck(date.getDate())}일</span>
+                    <img src={emojiArr[emoji]}/>
                 </div>
-                <div>{content}</div>
+
+                <div className="flex w-[75%] flex-col justify-between mb-5">
+
+                <br/>
+                    <div>{content}</div>
+                    <div className="flex">
+                        {photoList.map((photo, index) => (<img
+                                key={index}
+                                className="w-[200px] h-[150px] mt-5 mr-3"
+                                src={photo.path}
+                                alt={`Picture ${index + 1}`}/>))}
+                    </div>
+                </div>
             </div>
-            <div className="flex">
-                {photoList.map((photo, index) => (
-                    <img
-                        key={index}
-                        className="w-[200px] h-[150px] mt-5 mr-3"
-                        src={photo.path}
-                        alt={`Picture ${index + 1}`}/>
-                ))}
-            </div>
-        </div>
-    );
+
+        </div>);
 };
 
 export default DiaryListComponent;
--- client/src/pages/Diary/page.tsx
+++ client/src/pages/Diary/page.tsx
@@ -1,11 +1,15 @@
-import React, {useState} from 'react';
-import {Route, Routes, useNavigate} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import {Route, Routes, useNavigate, useParams} from "react-router-dom";
 import DiaryMomPage from "@/pages/Diary/DiaryMom/page";
 import DiaryBabyPage from "@/pages/Diary/DiaryBaby/page";
 
 
 const DiaryPage = () => {
+    const currParam = useParams()["*"];
     const [isToggled, setToggled] = useState(false);
+    useEffect(() => {
+        if(currParam=="baby") setToggled(true);
+    }, []);
     const navigate = useNavigate()
     const handleToggle = () => {
         setToggled(!isToggled);
