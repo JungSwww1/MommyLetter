39445aa033bb398aaee8159a6f8dc1472176b3bd
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2024-02-13 01:09:14 +0900

    ✨Feat: 스크롤 하단으로 내리는 작업

--- client/src/pages/DirectMessage/DirectMessageDetail/page.tsx
+++ client/src/pages/DirectMessage/DirectMessageDetail/page.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {ReactComponent as Camera} from '@/assets/icons/camera.svg';
 import {ReactComponent as Picture} from '@/assets/icons/picture.svg';
 import * as Stomp from "@stomp/stompjs";
@@ -7,7 +7,7 @@ import {enterDM, fetchChatList, fetchDMList, leaveDM} from "@/apis/DM/DMAPI";
 import {useParams} from "react-router-dom";
 import DirectMessage from "@/components/DirectMessage";
 import {getProfileAPI} from "@/apis/profile/ProfileAPI";
-
+import "./index.css";
 interface DMProps {
     senderId: number;
     receiverId: number;
@@ -24,16 +24,25 @@ interface DMInfoProps {
     "createdDate": string
 }
 
+
 const DirectMessageDetailPage = () => {
     const [chatList, setChatList] = useState<DMProps[]>([]);
     const [chat, setChat] = useState<string>("");
     let [client, setClient] = useState<Stomp.Client>();
     const [opponent, setOpponent] = useState<number>()
     const user = MommyLetterWS.getInstance().getUserInfo();
-    const roomNumber = useParams()["roomNumber"];
+    const roomNumber = Number(useParams()["roomNumber"]);
+    const chatUlRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+
+        connect(roomNumber);
+        return () => disConnect();
+    }, [roomNumber]);
     useEffect(() => {
 
         fetchDMList(Number(user["userId"])).then((response) => {
+
             const result = response.filter((info: DMInfoProps) => info.chatGroupId == Number(roomNumber))[0];
             const chatGroupName = result.chatRoomName.split("_")
             const opponentId = chatGroupName[2] == user["userId"] ? Number(chatGroupName[1]) : Number(chatGroupName[2]);
@@ -69,25 +78,14 @@ const DirectMessageDetailPage = () => {
         })
         console.log(chatList);
     }, []);
-    useEffect(() => {
 
-        // 최초 렌더링 시 , 웹소켓에 연결
-        connect();
 
-        return () => {
-            disConnect();
-            if (opponent) leaveDM(Number(user["userId"]), opponent)
-        }
-    }, []);
 
-    useEffect(() => {
-
-    }, [chatList]);
-
-    const connect = () => {
+    const connect = (roomNumber:number) => {
+        console.log(roomNumber);
         const clientdata = new Stomp.Client({
             brokerURL: "ws://localhost:8080/ws",
-            connectHeaders: MommyLetterWS.getInstance().header,
+            connectHeaders: {},
             debug: function (str) {
                 console.log(str);
             },
@@ -106,37 +104,31 @@ const DirectMessageDetailPage = () => {
         setClient(clientdata); // 클라이언트 갱신
 
     }
-    const callback = function (message: any) {
+    const callback = function (message:any) {
         if (message.body) {
             let msg = JSON.parse(message.body);
-
+            setChatList((chats:DMProps[]) => [...chats, msg]);
         }
+        console.log(chatList);
+
     };
 
 
+
     const sendChat = () => {
-        if (!chat || !user["userId"] || !opponent) {
+        if (chat === "") {
             return;
         }
-        const msg: DMProps = {
-            senderId: Number(user.userId),
-            receiverId: opponent,
-            content: chat,
-            chatGroupId: Number(roomNumber),
-            nickname: user["userId"],
-            profilePhoto: "",
-            createdDate: new Date().toString()
-        };
-
-        // 서버로 메시지 전송
         client?.publish({
-            destination: "/pub/message", body: JSON.stringify(msg),
+            destination: "/pub/message",
+            body: JSON.stringify({
+                senderId:user["userId"],
+                receiverId:opponent,
+                content:chat,
+                chatGroupId:roomNumber,
+            }),
         });
 
-        // 실시간으로 채팅 내용 반영
-        setChatList((prevChatList: DMProps[]) => [...prevChatList, msg]);
-
-        // 채팅 입력 필드 초기화
         setChat("");
     };
 
@@ -149,27 +141,55 @@ const DirectMessageDetailPage = () => {
         client?.deactivate();
     };
 
+
+    useEffect(() => {
+        const scrollToBottom = () => {
+            if (chatUlRef.current) {
+                chatUlRef.current.scrollTop = chatUlRef.current.scrollHeight;
+            }
+        };
+
+        // 새로운 채팅이 추가될 때마다 스크롤을 아래로 이동시킵니다.
+        const observer = new MutationObserver(scrollToBottom);
+        if (chatUlRef.current) {
+            observer.observe(chatUlRef.current, { childList: true });
+        }
+
+        // 컴포넌트가 언마운트될 때 옵저버를 정리합니다.
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
+
+    // 초기 렌더링 시 스크롤을 아래로 이동시킵니다.
+    useEffect(() => {
+        if (chatUlRef.current) {
+            chatUlRef.current.scrollTop = chatUlRef.current.scrollHeight;
+        }
+    }, []);
     return (<div className="flex flex-col h-[100%] w-[100%]">
-            <div className="h-[95%]">
-                {chatList.map((chat,index) => (
-                    <DirectMessage key={index} type={chat.senderId === Number(user["userId"])} name={chat.nickname} time={new Date(chat.createdDate).toString()}
-                                   message={chat.content} status={""} imageUrl={"/assets/images/potato.png"}
-                    />))}
+        <div
+            ref={chatUlRef} className="flex flex-col h-[95%] scrollBar chat_ul overflow-y-auto">
+            {chatList.map((chat, index) => (
+                <DirectMessage key={index} type={chat.senderId === Number(user["userId"])} name={chat.nickname}
+                               time={new Date(chat.createdDate).toString()}
+                               message={chat.content} status={""} imageUrl={"/assets/images/potato.png"}
+                />))}
 
 
+        </div>
+
+        <div className="fixed bottom-0 h-[5%] w-[40%] ">
+            <div className="flex flex-row items-center w-[100%] h-[100%] bg-gray-300 rounded-2xl p-5">
+                <button><Camera className="mr-3"/></button>
+                <input type="text" onChange={(e) => setChat(e.target.value)} className="w-[90%] bg-gray-300"
+                       placeholder="메시지 보내기..."
+                       value={chat}/>
+                <button><Picture/></button>
+                <button onClick={sendChat}>확인</button>
             </div>
-
-            <div className="absolute bottom-0 h-[5%] w-[40%] ">
-                <div className="flex flex-row items-center w-[100%] h-[100%] bg-gray-300 rounded-2xl p-5">
-                    <button><Camera className="mr-3"/></button>
-                    <input type="text" onChange={(e) => setChat(e.target.value)} className="w-[90%] bg-gray-300"
-                           placeholder="메시지 보내기..."
-                           value={chat}/>
-                    <button><Picture/></button>
-                    <button onClick={sendChat}>확인</button>
-                </div>
-            </div>
-        </div>);
+        </div>
+    </div>);
 };
 
 export default DirectMessageDetailPage;
