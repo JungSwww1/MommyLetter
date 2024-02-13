d2aca9ffc0b9d21ed2325957dc0e6c1adcb0fbd4
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2024-02-13 15:25:44 +0900

    💄Design: send이미지 추가

--- client/src/components/Search/Nickname/index.tsx
+++ client/src/components/Search/Nickname/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import {Link, useNavigate} from "react-router-dom";
+import {ReactComponent as Send} from "@/assets/icons/send.svg";
 interface NicknameProps {
     userId: number,
     nickname: string,
@@ -9,18 +10,25 @@ interface NicknameProps {
     follower: number,
     following: number
 }
-
 const NicknameComponent: React.FC<{ nicknameList: NicknameProps[] }> = ({ nicknameList }) => {
+    const navigate = useNavigate();
+    const move= (userId:number) => {
+        navigate(`/profile/${userId}`)
+    }
+    const onSelect = (e:any) =>{
+        console.log(e);
+    }
     return (
         <div className="w-[100%]">
+            <input type="file" onChange={onSelect} />
             {nicknameList.map((nicknameItem, index) => (
-                <div key={index} className="flex justify-around h-[10%] w-[100%]">
-                    <img src={nicknameItem.profilePhoto} className="w-[15%] mr-5 rounded-full"/>
-                    <p>{nicknameItem.nickname}</p>
+                <div key={index} className="flex justify-between items-center ml-5 h-[10%] w-[100%] " >
+                    <img src={nicknameItem.profilePhoto} className="w-[15%]  rounded-full"/>
+                    <button onClick={()=>move(nicknameItem.userId)} className="duraaation-300 active:scale-95">{nicknameItem.nickname}</button>
+                    <Link to="/message"  className="hover:scale-125 duration-300 w-[10%] h-[10%]"><button><Send/></button></Link>
                 </div>
             ))}
         </div>
     );
 };
-
 export default NicknameComponent;
