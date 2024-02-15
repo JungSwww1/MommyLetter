22036cc63e5f18447effb1495130ad558ca6ec49
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2024-02-15 15:16:01 +0900

    Merge branch 'frontend' into feat_seokju2

--- client/src/components/Search/HashTag/index.tsx
+++ client/src/components/Search/HashTag/index.tsx
@@ -8,9 +8,10 @@ const HashTagComponent: React.FC<{ hashtagList: HashtagProps[] }> = ({hashtagLis
     return (<div className="w-[100%]">
 
             {hashtagList.map((hashtagItem, index) => (
-                <div key={index} className="flex ml-5 h-[25%] w-[100%]">
+                <div key={index} className="flex ml-5 h-[10%] w-[100%]">
                     <span># {hashtagItem.content}</span>
                 </div>))}
+        {(!hashtagList) ||(hashtagList && hashtagList?.length<1) && <img src="/assets/images/not_found_search.jpg"/>}
     </div>);
 };
 
