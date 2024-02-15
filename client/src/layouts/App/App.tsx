bef7d31858aa08c0d6e50c78901d766b56a9fd74
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2024-02-15 13:20:31 +0900

    ✨Feat: 환경마다 다른 픽셀

--- client/src/layouts/App/App.tsx
+++ client/src/layouts/App/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import {Route, Routes, Navigate} from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from "@/pages/LoginPage/LoginPage";
 import Main from "@/layouts/Main";
 import UserRegist from "@/pages/UserRegist/UserRegist";
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 40%;
+  height: 100%;
+  background-color: white; 
+
+  /* 모바일 환경에서는 너비를 100%로 */
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
 
 function App() {
     const isAuth = () => {
@@ -10,7 +22,7 @@ function App() {
     };
 
     return (
-        <div className="w-[40%] h-[100%] bg-white">
+        <Container>
             <Routes>
                 {/* /login 경로로 이동할지 결정하는 조건부 라우팅 */}
                 <Route path="/login" element={!isAuth() ? <LoginPage/> : <Navigate to="/" replace />} />
@@ -22,6 +34,8 @@ function App() {
                 {/* 인증되지 않은 사용자를 /login 페이지로 리디렉션 */}
                 <Route path={"*"} element={<Navigate to="/login"/>}/>
             </Routes>
-        </div>);
+        </Container>
+    );
 }
+
 export default App;
